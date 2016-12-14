b5d3abfc274ac7b25c4b59db82e5f28138329d97
Elliott <00772604@leeds.gov.uk>
2016-12-14 13:55:42 +0000

    Added datepicker with no min date

--- app/assets/javascripts/breeze.js
+++ app/assets/javascripts/breeze.js
@@ -120,7 +120,23 @@
             onSelect: function (selected) {
                 $("input[data-type='datepicker-start']").datepicker("option", "maxDate", selected)
             }
-        });        
+        }); 
+        $("input[data-type='datepicker-start-nomin']").datepicker({
+            defaultDate: null,
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            onSelect: function(selected) {
+                $("input[data-type='datepicker-end-nomin']").datepicker("option", "minDate", selected)
+            }
+        });
+        $("input[data-type='datepicker-end-nomin']").datepicker({
+            defaultDate: null,
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            onSelect: function (selected) {
+                $("input[data-type='datepicker-start-nomin']").datepicker("option", "maxDate", selected)
+            }
+        });         
         $('.activitySearchHomeHeader a').click(function () {
             $('.activitySearchHomeContent').slideToggle("slow");
             $('.activitySearchHomeHeader').toggleClass("active");
