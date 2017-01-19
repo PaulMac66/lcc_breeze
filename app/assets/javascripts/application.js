1627fd43889cd9a4b0cd313d7d4d75e8c117c65f
Travis CI <developer@leeds.gov.uk>
2017-01-19 11:32:01 +0000

    added js for events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,24 +8,7 @@
 //= require date-range-filter
 //= require keyword-filter
 //= require news-archive-filter
+//= require event-Future-Dates-Hide
 
 
-
-      jQuery(document).ready(function () {
-      jQuery('ul.date-list').each(function () {
-      var LiN = jQuery(this).find('li').length;
-      if (LiN > 3) {
-      jQuery('li', this).eq(2).nextAll().hide().addClass('toggleable');
-      jQuery(this).append('<a class="plusMinus">Show more...</a>');
-      }
-      });
-      jQuery('ul.date-list').on('click', '.plusMinus', function () {
-      if (jQuery(this).hasClass('active')) {
-      jQuery(this).text('Show more...').removeClass('active');
-      } else {
-      jQuery(this).text('Show less...').addClass('active');
-      }
-      jQuery(this).siblings('li.toggleable').slideToggle();
-      });
-      });
  
---
+++ app/assets/javascripts/event-Future-Dates-Hide.js
@@ -0,0 +1,23 @@
+
+
+(function ($) {
+    
+    $(document).ready(function () {
+              jQuery('ul.date-list').each(function () {
+          var LiN = jQuery(this).find('li').length;
+          if (LiN > 3) {
+              jQuery('li', this).eq(2).nextAll().hide().addClass('toggleable');
+              jQuery(this).append('<a class="plusMinus">Show more...</a>');
+          }
+      });
+      jQuery('ul.date-list').on('click', '.plusMinus', function () {
+          if (jQuery(this).hasClass('active')) {
+              jQuery(this).text('Show more...').removeClass('active');
+          } else {
+              jQuery(this).text('Show less...').addClass('active');
+          }
+          jQuery(this).siblings('li.toggleable').slideToggle();
+      });
+      
+    });
+})(jQuery);
